379a2e3e521857cc463e5213c509c0589eb69049
Agus <agustinmohor@gmail.com>
2025-12-18 16:29:24 -0300

    correccion fondo menu pausa

--- src/Pause.cpp
+++ src/Pause.cpp
@@ -3,7 +3,7 @@
 PauseScene::PauseScene():m_font("../assets/fonts/fuente.ttf"),m_text(m_font,"Pause"){
     m_text.setCharacterSize(100);
     m_text.setPosition(sf::Vector2f(300,150));
-    overlay.setSize({800.f,800.f});
+    overlay.setSize({6000.f,6000.f});
     overlay.setFillColor(sf::Color(0,0,0,170));
 }
 
@@ -16,6 +16,7 @@ void PauseScene::backMatch(Game &m_gam){
 }
 
 void PauseScene::draw(sf::RenderWindow &m_win){
+    overlay.setPosition(sf::Vector2f(-2000,-2000));
     m_win.draw(overlay);
     m_win.draw(m_text);
 }
